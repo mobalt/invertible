07ef0b873bdf46fad37b30db7c0c2994e099f9e9
Moises Baltazar <me@moisesb.com>
2019-08-14 16:55:17 -0500

    More easily create symmetrical invertible functions

---
+++ src/simple.js
@@ -0,0 +1,44 @@
+import invertible_fn from './main'
+import { isInvertibleFn } from './checks'
+import { findProp } from './helpers'
+
+export function simple_inv({ context, fn }) {
+    const forwardCx = {},
+        backCx = {}
+
+    for (let prop in context) {
+        const v = context[prop]
+        if (Array.isArray(v)) {
+            forwardCx[prop] = v[0]
+            backCx[prop] = v[1]
+        } else if (typeof v == 'object') {
+            forwardCx[prop] = findProp(v, [
+                'value',
+                'val',
+                'v',
+                'f',
+                'forward',
+                'forwards',
+                'n',
+                'norm',
+                'normal',
+            ])
+            backCx[prop] = findProp(v, [
+                'inv',
+                'i',
+                'inverse',
+                'reverse',
+                'rev',
+                'r',
+                'back',
+                'backward',
+                'backwards',
+            ])
+        } else if (isInvertibleFn(v)) {
+            forwardCx[prop] = v
+            backCx[prop] = v.inv
+        }
+    }
+
+    return invertible_fn(fn.bind(forwardCx), fn.bind(backCx))
+}
---
+++ test/simple.spec.js
@@ -0,0 +1,31 @@
+import 'chai/register-should'
+import { simple_inv } from '../src/simple'
+
+describe('#simple_inv', () => {
+    const context = {
+        from: ['x', 'y'],
+        to: ['y', 'x'],
+        addend: [7, -7],
+        multiplicand: [2, 0.5],
+    }
+    function make_me_simple(inputObj) {
+        const inputValue = inputObj[this.from]
+        const resultValue = (inputValue + this.addend) * this.multiplicand
+
+        const result = {}
+        result[this.to] = resultValue
+        return result
+    }
+
+    const simple = simple_inv({
+        context,
+        fn: make_me_simple
+    })
+
+    it('can do forwards', () => {
+        simple({ x: 3 }).should.deep.equal({ y: 20 })
+    })
+    it ('can do backwards', ()=>{
+        simple.inv({ y: 17 }).should.deep.equal({ x: 5 })
+    })
+})
